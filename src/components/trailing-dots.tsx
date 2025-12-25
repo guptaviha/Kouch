9f47e65a47a240a24bfcbe9149c41e21f33bca1f
Karan Rajpal <amigokaran@gmail.com>
2025-12-24 20:58:01 -0500

    trailing dots animation

--- src/components/trailing-dots.tsx
+++ src/components/trailing-dots.tsx
@@ -1,9 +1,9 @@
 export default function TrailingDots() {
   return (
     <>
-      <span className="animate-bounce delay-75">.</span>
-      <span className="animate-bounce delay-150">.</span>
-      <span className="animate-bounce delay-300">.</span>
+      <span className="inline-block text-[1.1rem] ml-1 animate-bounce delay-75">.</span>
+      <span className="inline-block text-[1.1rem] ml-1 animate-bounce delay-150">.</span>
+      <span className="inline-block text-[1.1rem] ml-1 animate-bounce delay-300">.</span>
     </>
   );
 }
